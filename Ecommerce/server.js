fd79cf9e3aaeeddc72abbe4efd3df51d51516d42
Prats222 <126672828+Prats222@users.noreply.github.com>
2023-09-20 13:43:06 +0530

    ,

--- Ecommerce/server.js
+++ Ecommerce/server.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
+import path from 'path';
 //configure env
 dotenv.config();
 
@@ -20,6 +21,7 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 //all routes
 app.use('/api/v1/auth',authRoutes);
@@ -29,11 +31,15 @@ app.use('/api/v1/category',categoryRoutes);
 app.use('/api/v1/product',productRoutes);
 
 //rest api
-app.get('/',(req,res)=>{
-    res.send({
-        message: "Welcome to my app"
-    })
+// app.get('/',(req,res)=>{
+//     res.send({
+//         message: "Welcome to my app"
+//     })
+// })
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
+
 const PORT=process.env.PORT || 8080;
 app.listen(PORT,()=>{
     console.log(`Server running on ${PORT}`.bgCyan.white);
